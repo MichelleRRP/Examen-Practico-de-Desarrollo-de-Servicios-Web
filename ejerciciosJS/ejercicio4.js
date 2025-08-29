f87e4dd6d2e6013295dce876d8b380a09f948fc6
DiegoPeralta22 <luisdiegoperaltaperez@gmail.com>
2025-08-28 21:50:00 -0700

    Create ejercicio4.js

---
+++ ejerciciosJS/ejercicio4.js
@@ -0,0 +1,24 @@
+
+// Crear un arreglo de números desordenados
+let numeros = [15, 3, 9, 5, 12, 7, 1, 20, 8];
+
+// Ordenar el arreglo de menor a mayor utilizando el método sort()
+numeros.sort(function(a, b) {
+    return a - b; // Si a < b, el resultado será negativo y el orden se mantendrá.
+});
+
+// Mostrar el arreglo ordenado
+console.log("Arreglo ordenado de menor a mayor:", numeros);
+
+
+// Crear un arreglo de números desordenados
+let numeros = [15, 3, 9, 5, 12, 7, 1, 20, 8];
+
+// Ordenar el arreglo de menor a mayor utilizando el método sort()
+numeros.sort(function(a, b) {
+    return a - b; // Si a < b, el resultado será negativo y el orden se mantendrá.
+});
+
+// Mostrar el arreglo ordenado
+console.log("Arreglo ordenado de menor a mayor:", numeros);
+
